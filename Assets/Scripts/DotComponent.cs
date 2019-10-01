8c1ed470d48cf6af88efb6d2d3a1d2a21e1c5389
Dheeraj <dheeraj.p@codewalla.com>
2019-10-01 10:03:15 +0530

    Use Mouse Clicks: to change color of dot

--- Assets/Scripts/DotComponent.cs
+++ Assets/Scripts/DotComponent.cs
@@ -2,32 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using Color = UnityEngine.Color;
 //using Color = System.Drawing.Color;
-public class DotComponent : MonoBehaviour
+public class DotComponent : MonoBehaviour, IPointerDownHandler
 {
     public GameObject dotComponent;
     readonly List<Color> colorList = new List<Color>()
      {
          Color.red,
          Color.yellow,
-         Color.blue,
+         Color.blue
      };
 
     // Start is called before the first frame update
     void Start()
     {    
-        ChangeColor();
+        CreateRandomColor();
     }
 
-    public void ChangeColor()
+    public void CreateRandomColor()
     {       
         dotComponent.GetComponent<Image>().color = colorList[Random.Range(0, colorList.Count)];
+    }
+
+    public void ChangeColorOnTouch()
+    {
+        dotComponent.GetComponent<Image>().color = Color.black;
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        ChangeColorOnTouch();
+        
     }
 }
