9a412e15b3499b2178e8f682c7cdb79827437477
Dheeraj <dheeraj.p@codewalla.com>
2019-10-03 09:55:37 +0530

    phone specific UI changes

--- Assets/Scripts/DotController.cs
+++ Assets/Scripts/DotController.cs
@@ -16,12 +16,12 @@ public class DotController : MonoBehaviour
 
     public void CreateGrid()
     {
-        for (int row = 0; row < 5; row++)
+        for (int row = 0; row < 8; row++)
         {
-            for (int col = 0; col < 8; col++)
+            for (int col = 0; col < 5; col++)
             {
-                GameObject currentDot = Instantiate(dotPrefab, new Vector3(dotPrefab.transform.position.x + (col * 1.5f),
-                dotPrefab.transform.position.y + (row * -1.5f), canv.transform.position.z), Quaternion.identity) as GameObject;
+                GameObject currentDot = Instantiate(dotPrefab, new Vector3(dotPrefab.transform.position.x + (col * 0.7f),
+                dotPrefab.transform.position.y + (row * -0.7f), canv.transform.position.z), Quaternion.identity) as GameObject;
                 currentDot.transform.SetParent(canv.transform);
             }
         }
